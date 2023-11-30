7caba9f3b908cabfc04833c0afc7faef98b98590
kloxer <102638907+kloxer@users.noreply.github.com>
2023-11-29 20:36:21 -0500

    Implemented ceiling light turn off

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-
-
-// async function checkAPI() {
-//     try {
-//         const response = await axios.get(`${HOME_ASSISTANT_URL}/api/`, {
-//             headers: {
-//                 'Authorization': `Bearer ${HOME_ASSISTANT_API_KEY}`,
-//                 "content-type": "application/json",
-//             }
-//         });
-//         console.log("Successfully connected to Home Assistant API:", response.data);
-//     } catch (error) {
-//         console.error('Error connecting to Home Assistant API:', error.response ? error.response.data : error.message);
-//     }
-// }
-
-// async function fetchEntities() {
-//     try {
-//         const response = await axios.get(`${HOME_ASSISTANT_URL}/api/states`, {
-//             headers: {
-//                 'Authorization': `Bearer ${HOME_ASSISTANT_API_KEY}`,
-//                 "content-type": "application/json",
-//             }
-//         });
-//         return response.data; // This will be an array of all entities
-//     } catch (error) {
-//         console.error('Error fetching entities:', error.response ? error.response.data : error.message);
-//         return [];
-//     }
-// }
-
-// async function listControllableDevices() {
-//     const entities = await fetchEntities();
-//     entities.forEach(entity => {
-//         if (entity.entity_id.startsWith('light.') || entity.entity_id.startsWith('switch.')) {
-//             console.log(`Entity ID: ${entity.entity_id}, State: ${entity.state}`);
-//         }
-//     });
-// }
-
-//listControllableDevices();
 const HOME_ASSISTANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmZjNhZTRiOTRhMTg0Y2ViYmIzMWNkNDg4N2MzMzlkMSIsImlhdCI6MTcwMTI5Mzc3NiwiZXhwIjoyMDE2NjUzNzc2fQ.tKXrI9PeMTKTAm9Pz3SBYEW1bVfNpKf7XNV12mzw1Co";
 const HOME_ASSISTANT_URL = "https://socaresrpi.duckdns.org:8123";
+
+async function fetchEntities() {
+    try {
+        const response = await fetch(`${HOME_ASSISTANT_URL}/api/states`, {
+            headers: {
+                'Authorization': `Bearer ${HOME_ASSISTANT_API_KEY}`,
+                'Content-Type': 'application/json',
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json(); // This will be an array of all entities
+    } catch (error) {
+        console.error('Error fetching entities:', error);
+        return [];
+    }
+}
+
+async function listControllableDevices() {
+    const entities = await fetchEntities();
+    entities.forEach(entity => {
+        if (entity.entity_id.startsWith('light.') || entity.entity_id.startsWith('switch.')) {
+            console.log(`Entity ID: ${entity.entity_id}, State: ${entity.state}`);
+        }
+    });
+}
+
+listControllableDevices();
 
 async function setColor(entityId, color) {
     try {
@@ -56,11 +44,39 @@ async function setColor(entityId, color) {
                 color_name: color
             })
         });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const data = await response.json();
+        turnOffMain();
         console.log(`Set ${entityId} to ${color}:`, data);
     } catch (error) {
         console.error(`Error setting color of ${entityId}:`, error);
     }
 }
+
+async function turnOffMain(){
+    entityId= "switch.bedroom_light";
+    try {
+        const url = `${HOME_ASSISTANT_URL}/api/services/switch/turn_off`;
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${HOME_ASSISTANT_API_KEY}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                entity_id: entityId
+                        })
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Turned off '+ entityId);
+    } catch (error) {
+        console.error(`Error turning off ${entityId}:`, error);
+    }
+}
 // Example usage
-setColor('light.controller_rgb_ir_cfdb5f', 'green');
+setColor('light.controller_rgb_ir_cfdb5f', 'blue');
